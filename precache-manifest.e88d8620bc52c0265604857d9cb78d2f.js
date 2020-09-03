880b56642b0dffb3694d1b5268096da18ef2ca45
kamalwc <carmichaelkw@gmail.com>
2020-09-03 09:34:34 -0400

    Updates

---
+++ precache-manifest.e88d8620bc52c0265604857d9cb78d2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3188a61da238e0c00b972bee4dec9323",
+    "url": "/updatedPortfolio/index.html"
+  },
+  {
+    "revision": "1d5d32f45d0404414f7d",
+    "url": "/updatedPortfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f7b330f21b53dd5c7a9a",
+    "url": "/updatedPortfolio/static/js/2.5c395735.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/updatedPortfolio/static/js/2.5c395735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d5d32f45d0404414f7d",
+    "url": "/updatedPortfolio/static/js/main.8e16c7cb.chunk.js"
+  },
+  {
+    "revision": "378aefda62beda118c44",
+    "url": "/updatedPortfolio/static/js/runtime-main.0bec6ce2.js"
+  }
+]);
